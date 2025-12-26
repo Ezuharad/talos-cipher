a1f4434dfc68398455e29663c997f6669fb43ccf
Steven Chiacchira <schiacchira@Gmail.com>
2025-12-26 12:14:28 -0500

    Implemented bitwise operators for bits::Bit

--- src/bits/bit.rs
+++ src/bits/bit.rs
@@ -1,4 +1,5 @@
 // 2025 Steven Chiacchira
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign};
 
 /// Represents a single bit.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -23,5 +24,47 @@ impl Bit {
 impl From<bool> for Bit {
     fn from(is_set: bool) -> Self {
         Self(is_set)
+    }
+}
+
+impl BitAnd for Bit {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Bit(self.0 && rhs.0)
+    }
+}
+
+impl BitOr for Bit {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Bit(self.0 || rhs.0)
+    }
+}
+
+impl BitXor for Bit {
+    type Output = Self;
+
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Bit(self.0 != rhs.0)
+    }
+}
+
+impl BitAndAssign for Bit {
+    fn bitand_assign(&mut self, rhs: Self) {
+        self.0 = self.0 && rhs.0;
+    }
+}
+
+impl BitOrAssign for Bit {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.0 = self.0 || rhs.0;
+    }
+}
+
+impl BitXorAssign for Bit {
+    fn bitxor_assign(&mut self, rhs: Self) {
+        self.0 = self.0 != rhs.0;
     }
 }
